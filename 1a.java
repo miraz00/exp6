07197971f797fbbcedad47dab944996d4726bb18
miraz00 <42742560+miraz00@users.noreply.github.com>
2022-02-14 05:58:45 +0530

    Create 1a.java

---
+++ 1a.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class main
+{
+	public static void main(String args[])
+	{
+		try
+		{
+		int a=5/0;
+		System.out.println(a);
+		}
+		catch(ArithmeticException e)
+		{
+		  System.out.println(e);
+		}
+	}
+	
+ }
